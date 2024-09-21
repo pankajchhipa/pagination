4232ee90dac1f59144257367c1ba1d007eca8a8e
pankajchhipa <pankajchhipa1997@gmail.com>
2024-09-21 08:58:51 +0530

    update files

--- src/main/java/com/practice/demo/model/ExceptionCode.java
+++ src/main/java/com/practice/demo/model/ExceptionCode.java
@@ -1,6 +1,6 @@
 package com.practice.demo.model;
 
-import jdk.jfr.StackTrace;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/practice/demo/services/HotelServiceImpl.java
+++ src/main/java/com/practice/demo/services/HotelServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -39,7 +40,7 @@ public class HotelServiceImpl implements HotelService {
                 Sort.by("hotelid").ascending());
         Page<HotelEntity> pagedResult = repository.findAll(paging);
 
-        List<HotelEntity> objects = pagedResult.hasContent() ? pagedResult.getContent() : List.of();
+        List<HotelEntity> objects = pagedResult.hasContent() ? pagedResult.getContent() : new ArrayList<>();
         List<HotelEntity> all = repository.findAll();
         return  HotelResponse.builder().hotel(objects).totalData(all.size()).build();
     }
